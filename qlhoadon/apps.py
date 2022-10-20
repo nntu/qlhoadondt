b2e7462327ee064e655f1872213bff7507d90b6e
ngoctuct-home <ngoctuct@gmail.com>
2022-10-20 21:18:22 +0700

    first commit

---
+++ qlhoadon/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QlhoadonConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "qlhoadon"
---
+++ qlhoadon/models.py
@@ -0,0 +1,28 @@
+from logging.config import stopListening
+from django.db import models
+
+# Create your models here.
+
+class DonVi(models.Model):
+    MaSoThue = models.CharField(max_length=14)
+    TenDonVi = models.CharField(max_length=255)
+    DiaChi = models.CharField(max_length=255)
+    SoDienThoai = models.CharField(max_length=20)
+    
+
+
+class HoaDonDienTu(models.Model):
+    
+    MaSoThue = models.CharField(max_length=14)
+    KyHieu = models.CharField(max_length=255)
+    So = models.CharField(max_length=255)
+    NgayHoaDon = models.DateField()
+    SoTien = models.DecimalField()
+    DiaChiTraCuu = models.CharField(max_length=255)
+    MaSoBiMat = models.CharField(max_length=255)    
+    NguoiThanhToan  = models.CharField(max_length=255)
+    NgayNhap =models.DateField()    
+    DaThanhToan = models.BooleanField()
+    NgayThanhToan = models.DateField()
+    
+
